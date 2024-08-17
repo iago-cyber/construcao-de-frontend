f894f75f08df2a0f9d423d91f836bade0c813b9c
iago-cyber <iagoabadia1@gmail.com>
2024-08-16 21:50:25 -0300

    criação da aula02

---
+++ Aulas/aula02/index.js
@@ -0,0 +1,53 @@
+const root = document.getElementById("root");
+
+//componentes da aplicação
+function Cabecalho() {
+    const header = document.createElement("header");
+    return header;
+}
+
+function Principal() {
+    const main = document.createElement("main");
+    return main;
+}
+
+function Rodape() {
+    const footer = document.createElement("footer");
+    return footer;
+}
+
+function formLogin() {
+    const button = Botao("Entrar");
+    const form = document.createElement("form");
+    form.setAttribute("action", "");
+    form.setAttribute("method", "post");
+    form.append(button);
+    return form;
+}
+
+function Botao(nome) {
+    const button = document.createElement("input");
+    button.setAttribute("type", "submit");
+    button.setAttribute("value", nome);
+    return button;
+}
+
+//paginas da aplicação
+function Perfil() {
+    root.append(Cabecalho());
+    root.append(Principal());
+}
+
+function Home() {
+    root.append(Cabecalho());
+    root.append(Principal());
+}
+
+function Login() {
+    const main = Principal();
+    main.append(formLogin());
+    root.append(main);
+    root.append(Rodape());
+}
+
+Login();
