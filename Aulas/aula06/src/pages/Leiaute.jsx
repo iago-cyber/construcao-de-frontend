8f08db33a1d45cb2f3b2300121609c2f59c8eabb
iago-cyber <iagoabadia1@gmail.com>
2024-09-24 21:45:46 -0300

    inclusão da aula06

---
+++ Aulas/aula06/src/pages/Leiaute.jsx
@@ -0,0 +1,26 @@
+import { Outlet, NavLink } from "react-router-dom";
+import "./Leiaute.css";
+
+function Leioute() {
+    return (
+        <>
+            <header>
+                <h1>Aluno Online</h1>
+            </header>
+            <main>
+                <nav>
+                    <ul>
+                        <li><NavLink to="/home">Home</NavLink></li>
+                        <li><NavLink to="/perfil">Perfil</NavLink></li>
+                        <li><NavLink to="/">Sair</NavLink></li>
+                    </ul>
+                </nav>
+                <section>
+                    <Outlet />
+                </section>
+            </main>
+        </>
+    );
+}
+
+export default Leioute;
