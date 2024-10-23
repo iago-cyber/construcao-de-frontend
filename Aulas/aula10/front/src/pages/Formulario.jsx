bf0fb82b96f0998ab75bef97c29c5057ef5af71c
iago-cyber <iagoabadia1@gmail.com>
2024-10-22 21:55:19 -0300

    inclusão da aula 10

---
+++ Aulas/aula10/front/src/pages/Formulario.jsx
@@ -0,0 +1,16 @@
+import { useForm } from "react-hook-form";
+import InputTelefone from "../components/InputTelefone";
+import InputNome from "../components/InputNome";
+
+function Formulario(props) {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    return (
+        <form onSubmit={handleSubmit(props.trataEnviar)}>
+            <InputNome register={register} error={errors.nome} />
+            <InputTelefone register={register} error={errors.telefone} />
+            <button type="submit">Salvar</button>
+        </form>
+    )
+}
+
+export default Formulario;
---
+++ Aulas/aula10/front/src/services/ContatoService.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const url = import.meta.env.VITE_URL_API
+
+function carregarContatos() {
+  const dados = [];
+  axios
+    .get(import.meta.env.VITE_URL_API)
+    .then((response) => dados.push(...response.data))
+    .catch((error) => new Error("Deu ruim"));
+  return dados;
+}
+
+function criarContato(contato) {
+  return axios
+    .post(url, contato)
+    .then(
+      (response) => { return { sucesso: true, dados: response.data } }
+    )
+    .catch(
+      (error) => { return { sucesso: false, mensage: error.message } }
+    );
+
+  /*try {
+  const response = await axios.post(url, contato)
+} catch (error) {
+  return { sucesso: false, mensage: error.message }
+}*/
+}
+
+function atualizarContato(contato) { }
+
+function removerContato(contato) { }
+
+function obterContato(contato) { }
+
+export { carregarContatos, criarContato }
